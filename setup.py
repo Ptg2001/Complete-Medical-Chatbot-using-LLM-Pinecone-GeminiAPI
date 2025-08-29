16cf71dcd4bace80c0a3599355ca67f86472bb28
ptg2001 <piyush.hole2001@gmail.com>
2025-08-29 22:12:29 +0530

    Medical Chatbot: Vercel UI + Serverless API

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Piyush Hemant Hole",
+    author_email="piyush.hole2001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
